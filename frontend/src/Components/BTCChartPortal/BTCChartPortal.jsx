c5f5984662a6bbc80a2affbbc9e9073157454baa
Abdul <abdullahmd326@gmail.com>
2022-07-25 02:40:03 -0400

    Adding Bitcoin chart functionality

--- frontend/src/Components/BTCChartPortal/BTCChartPortal.jsx
+++ frontend/src/Components/BTCChartPortal/BTCChartPortal.jsx
@@ -1,28 +1,80 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
 
 const BTCChartPortal = () => {
 
-    let result = null;
+    const time = ["1 day", "14 days", "30 days"];
     const URL= "https://api.coingecko.com/api/v3";
     const id = "bitcoin";
-    const date = '04-05-2021'; // Sample date goes here, will include a dynamic way to add it later
-    const API_ENDPOINT = "/coins/" + id + "/history?date=" + date;
+    const QUERY_STRING = "?vs_currency=usd&days=14"; // Default selection for now.
+    const API_ENDPOINT = "/coins/" + id + "/market_chart" + QUERY_STRING + "&interval=daily";
 
+    const [chartData, setChartData] = useState({});
+
+    // Default implementation for now.. 14 days
     useEffect(() => {
         fetch(URL + API_ENDPOINT)
         .then(response => response.json())
         .then(res => {
-            result = res; 
-            console.log(res);
+            setChartData(prevState => {
+                return {
+                    ...prevState,
+                    labels: ['1', '2', '3', '4', '5', '6','7','8','9','10', '11', '12', '13', '14', '15'],
+                    datasets: [
+                    {
+                        label: "Price in USD",
+                        data: res.prices.map((btcInfo) => btcInfo[1]),
+                        backgroundColor: [
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000",
+                            "#ff0000"
+                        ]
+                    }]
+                }
+            }); 
         })
         .catch(err => console.log(err));
-
     }, []);
 
     return (
         <div className='dashboard'>
-            { result === null ? "Not available" : result }
+            <div>
+                { time.map(element => { 
+                    return (
+                        <button style={{ marginTop: '1rem', marginRight: '1rem', marginBottom: '0.4rem' }} class="btn btn-secondary">{element}</button>
+                    );
+                })}
+                <div>
+                    <Line
+                        data={chartData}
+                        options={{
+                            plugins: {
+                                title: {
+                                display: true,
+                                text: "Bticoin Chart"
+                                },
+                                legend: {
+                                display: true,
+                                position: "bottom"
+                            }
+                        }
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
