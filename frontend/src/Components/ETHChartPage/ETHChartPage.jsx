1888658bdf2e877c874df89dff57a7e7936ac8d7
Abdul <abdullahmd326@gmail.com>
2022-07-31 01:57:47 -0400

    Different day/date ranges of ETH price chart info complete

--- frontend/src/Components/ETHChartPage/ETHChartPage.jsx
+++ frontend/src/Components/ETHChartPage/ETHChartPage.jsx
@@ -35,11 +35,7 @@ const ETHChartPage = () => {
     });    
   
     const [chartData, setChartData] = useState({});
-    let days = [];
-
-    for (var i = 0; i <= 14; i++) {
-      days.push(String(i + 1));
-    }
+    const [displayChart, updateDisplayChart] = useState('15');
 
   // Get current coin prices as well as historical prices
   useEffect(() => {
@@ -50,7 +46,8 @@ const ETHChartPage = () => {
         setChartData(prevState => {
           return {
             ...prevState,
-            res
+            res,
+            time: [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11, 12, 13, 14, 15]
           }
         });
       })
@@ -77,49 +74,112 @@ const ETHChartPage = () => {
     fetchCoins();
   }, []);
 
-  // Set display configurations
-  var data = {
-    labels: days,
-    datasets: [{
-      label: `Ethereum Price`,
-      data: chartData?.res?.prices?.map(x => x[1].toFixed(2)),
-      backgroundColor: 'red',
-      borderColor: 'red',
-      borderWidth: 1
-    }]
-  };
-
-  // Adding options to enhance charts
-  var options = {
-    maintainAspectRatio: false,
-    scales: {
-    },
-    legend: {
-      display: true,
-      position: "bottom",
-      labels: {
-        fontSize: 25
-      }
-    }
-  }
-  
-  // Buttons for displaying different chart date ranges
+  // Buttons for displaying different chart date ranges, used to update the diplay chart date, which triggers a re-render
   const buttonHandler = (d) => {
     switch(d) {
       case "Last Day":
+        updateDisplayChart("1");
         break;
+
       case "Last 14 Days":
+        updateDisplayChart("14");
         break;
+
       case "Last 30 Days":
+        updateDisplayChart("30");
         break;
+
       default:
         break;
     }
   }
 
+  // Dependency is used to update chart rendering each case is considered and a separate API call is made for each scenario
+  useEffect(() => {
+    const fetchCoins = async (value) => {
+      if (value === '14'){
+        await fetch(URL + ETHEREUM_PRICE_ENDPOINT)
+        .then(response => response.json())
+        .then(res => {
+          setChartData(prevState => {
+            return {
+              ...prevState,
+              res,
+              time: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+            }
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      }
+      else if (value === '30'){
+        await fetch(URL + "/coins/ethereum/market_chart?vs_currency=usd&days=30&interval=daily")
+        .then(response => response.json())
+        .then(res => {
+          setChartData(prevState => {
+            return {
+              ...prevState,
+              res,
+              time: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+                16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+            }
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      }
+      else {
+        await fetch(URL + "/coins/ethereum/market_chart?vs_currency=usd&days=1&interval=hourly")
+        .then(response => response.json())
+        .then(res => {
+          setChartData(prevState => {
+            return {
+              ...prevState,
+              res,
+              time: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+                16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+            }
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      }
+    }
+    fetchCoins(displayChart);
+  }, [displayChart]);
+
+    // Set display configurations
+    var data = {
+      labels: chartData?.time,
+      datasets: [{
+        label: `Ethereum Price`,
+        data: chartData?.res?.prices?.map(x => x[1].toFixed(2)),
+        backgroundColor: 'red',
+        borderColor: 'red',
+        borderWidth: 1
+      }]
+    };
+  
+    // Adding options to enhance charts
+    var options = {
+      maintainAspectRatio: false,
+      scales: {
+      },
+      legend: {
+        display: true,
+        position: "bottom",
+        labels: {
+          fontSize: 25
+        }
+      }
+    }
+    
   let buttonDaysArray = ["Last Day", "Last 14 Days", "Last 30 Days"];
   let buttons = buttonDaysArray.map(day =>  {
-    return <button onClick={buttonHandler} style={{marginRight: '1rem', paddingLeft: '0.5rem', paddingRight: '0.5rem'}} class="btn btn-secondary">{day}</button>
+    return <button onClick={() => buttonHandler(day)} style={{marginRight: '1rem', paddingLeft: '0.5rem', paddingRight: '0.5rem'}} class="btn btn-secondary">{day}</button>
   });
 
   // Display Title, 24 Hr. % Change, Price of Coin
@@ -130,6 +190,7 @@ const ETHChartPage = () => {
     return (
       <div>
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+          <h1>{displayChart === '15' || displayChart === '30' ? "Yessir" : 'No ser'}</h1>
           <h3 style={{marginTop: '2rem'}}>Ethereum Price: <b>${ethInfo.information.ethereum.usd} USD</b></h3> 
           <h5 style={{marginBottom: '2rem', display: 'inline'}}>24 Hr. % Change: 
             { ethInfo.information.ethereum.usd_24h_change < 0 ? 
@@ -138,7 +199,7 @@ const ETHChartPage = () => {
             }
           </h5>
           <div>
-            {( chartData === {} || days === [] ) ? <div>Loading...</div> : 
+            {( chartData === {} || chartData.time === [] ) ? <div>Loading...</div> : 
               <div style={{marginTop: '2rem'}}>
                 <Line 
                   data={data}
