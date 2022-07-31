adc19b5091be591a84eddac1b334058e097f9357
Abdul <abdullahmd326@gmail.com>
2022-07-31 00:56:27 -0400

    Making changes to sidebar/navbar

--- frontend/src/Components/Collections/ERC720.jsx
+++ frontend/src/Components/Collections/ERC720.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ERC720 = () => {
-    const URL = "https://api.etherscan.io/api";
+   const URL = "https://api.etherscan.io/api";
 
     return (
         <div className="erc-720-token-page">
--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -4,42 +4,38 @@ import '../Sidebar/Sidebar.css';
 const Sidebar = () => {
 
     return (
-        <div class="container-fluid">
-            <div class="row">
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
-                    <div class="sidebar-sticky pt-3">
-                        <table class="table table-striped table-dark">
-                            <tbody>
-                                <tr class="col-sm-3">
-                                    <td><img src={require("../../assets/images/dashboard.png")} width="35" height="25" alt="logo" /></td>
-                                    <td><a href="/dashboard">Dashboard</a></td>
-                                </tr>
-                                <tr>
-                                    <td><img src={require("../../assets/images/transactions.png")} width="35" height="25" alt="logo" /></td>
-                                    <td><a href="/transactions">Transactions</a></td>
-                                </tr>
-                                <tr>
-                                    <td><img src={require("../../assets/images/analytics.png")} width="35" height="25" alt="logo" /></td>
-                                    <td><a href="/analytics">Wallet Analytics</a></td>
-                                </tr>
-                                <tr>
-                                    <td><img src={require("../../assets/images/collections.png")} width="35" height="25" alt="logo" /></td>
-                                    <td><a href="/collections">Collections</a></td>
-                                </tr>
-                                <tr>
-                                    <td><img src={require("../../assets/images/charts.png")} width="35" height="25" alt="logo" /></td>
-                                    <td><a href="/charts">Charts</a></td>
-                                </tr>
-                                <tr>
-                                    <td><img src={require("../../assets/images/about.png")} width="35" height="25" alt="logo" /></td>
-                                    <td><a href="/about">About</a></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </nav>
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+            <div class="sidebar-sticky pt-3">
+                <table class="table table-striped table-dark">
+                    <tbody>
+                        <tr class="col-sm-3">
+                            <td><img src={require("../../assets/images/dashboard.png")} width="35" height="25" alt="logo" /></td>
+                            <td><a href="/">Dashboard</a></td>
+                        </tr>
+                        <tr>
+                            <td><img src={require("../../assets/images/transactions.png")} width="35" height="25" alt="logo" /></td>
+                            <td><a href="/transactions">Transactions</a></td>
+                        </tr>
+                        <tr>
+                            <td><img src={require("../../assets/images/analytics.png")} width="35" height="25" alt="logo" /></td>
+                            <td><a href="/walletAnalytics">Wallet Analytics</a></td>
+                        </tr>
+                        <tr>
+                            <td><img src={require("../../assets/images/collections.png")} width="35" height="25" alt="logo" /></td>
+                            <td><a href="/collections">Collections</a></td>
+                         </tr>
+                        <tr>
+                            <td><img src={require("../../assets/images/charts.png")} width="35" height="25" alt="logo" /></td>
+                            <td><a href="/charts">Charts</a></td>
+                        </tr>
+                        <tr>
+                            <td><img src={require("../../assets/images/about.png")} width="35" height="25" alt="logo" /></td>
+                            <td><a href="/about">About</a></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-        </div>
+        </nav>
     );
 }
 
