173ab7717134d383df32ff45d9b8ee0d65b63379
Abdul <abdullahmd326@gmail.com>
2022-07-30 22:03:44 -0400

    Fixing ERC token pages

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -10,6 +10,7 @@ import Footer from '../Footer/Footer';
 import About from '../About/About';
 import Collections from '../Collections/Collections';
 import ERC720 from '../Collections/ERC720';
+import ERC721 from '../Collections/ERC721';
 import ERC1155 from '../Collections/ERC1155';
 import WalletStats from '../WalletStats/WalletStats';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -24,7 +25,7 @@ const App = () => {
         <div>
           <Sidebar />
         </div>
-        <div>
+        <div> 
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
             <Route exact path="/about" element={<About />}></Route>
@@ -33,7 +34,8 @@ const App = () => {
             <Route exact path="/ethChart" element={<ETHChartPage />}></Route>
             <Route exact path="/collections" element={<Collections />}></Route>
             <Route exact path="/erc720" element={<ERC720 />}></Route>
-            <Route exact path="/erc721" element={<ERC1155 />}></Route>
+            <Route exact path="/erc721" element={<ERC721 />}></Route>
+            <Route exact path="/erc1155" element={<ERC1155 />}></Route>
             <Route exact path="/walletAnalytics" element={<WalletStats />}></Route>
             <Route exact path="*" element={<PageNotFound />}></Route>
           </Routes>
---
+++ frontend/src/Components/Collections/ERC721.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ERC721 = () => {
+
+    return (
+        <div className="erc-721-token-page">
+
+        </div>
+    )
+}
+
+export default ERC721;
