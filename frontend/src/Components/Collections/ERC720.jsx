fc81bf5cd1c3085fd31bd45957755085df61bc04
Abdul <abdullahmd326@gmail.com>
2022-08-14 19:07:08 -0400

    Adding contract address check functionality

--- frontend/src/Components/Collections/ERC720.jsx
+++ frontend/src/Components/Collections/ERC720.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import { useNavigate } from 'react-router-dom';
+import Alert from '../Alert/Alert';
 import moment from 'moment';
 
 import {
@@ -52,10 +53,8 @@ ChartJS.register(
 const ERC720 = () => {
     const navigate = useNavigate();
 
-    const URL= "https://api.coingecko.com/api/v3";
     const [tokenContractAddress, updateContractAddress] = useState('0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'); // Set default to EthDev contract address 
-    const ERC20_INFO_ENDPOINT = '/coin/ethereum/contract/' + tokenContractAddress;
-    const ERC20_PRICE_ENDPOINT = '/coin/ethereum/contract/' + tokenContractAddress + '/market_chart?vs_currency=usd&days=0.05';
+    const [formAlert, updateAlert] = useState("");
 
     const [coinInfo, updateCoinInfo] = useState({
       information: null
@@ -66,6 +65,16 @@ const ERC720 = () => {
     // Update based on the toggle value afterwards, hence state will be used here
     const [astheticNaming, updateAstheticNaming] = useState("Ethereum"); // Naming for chart, default to Ethereum to start 
     
+    const URL= "https://api.coingecko.com/api/v3";
+    const ERC20_INFO_ENDPOINT = '/coins/ethereum/contract/' + tokenContractAddress;
+    const ERC20_PRICE_ENDPOINT = '/coins/ethereum/contract/' + tokenContractAddress + '/market_chart?vs_currency=usd&days=0.05';
+
+    /*
+        useState(id.includes("-") ? 
+                 id.split("-")[0].substring(0, 1).toUpperCase() + id.split("-")[0].substring(1, id.split("-")[0].length) : 
+                 id.substring(0, 1).toUpperCase() + id.substring(1, id.length)); // Naming for chart 
+    */
+
     // Get Ethereum data initially
     useEffect(() => {
       const fetchCoins = async () => {      
@@ -109,11 +118,47 @@ const ERC720 = () => {
     }, [])
 
 
+  const formHandler = async (e) => {
+    e.preventDefault();
+
+    if (tokenContractAddress.length === 42 && tokenContractAddress.substring(0, 2) === "0x"){
+
+      // Fetch token contract address to check the value 
+      await fetch(URL + ERC20_INFO_ENDPOINT)
+      .then(response => response.json())
+      .then(res => {
+        if (res.error){
+          updateAlert('invalid'); // Set alert if error is found to notify invalid contract address
+          localStorage.clear();
+        }
+        else {
+          updateAlert('');
+          localStorage.setItem("contractAddress", tokenContractAddress); // Update storage to hold smart contract, if valid.
+        }
+      })
+      .catch(err => {
+        console.log(err); // Display error if contract is not found
+        updateAlert('invalid');
+        localStorage.clear();
+      }); 
+    }
+    else {
+        if (formAlert === "invalid"){ // If the format is not of length 42 and start with 0x (hex), throw error
+            e.target.reset();
+            localStorage.clear();                
+        }
+        else {
+            updateAlert("invalid"); // If repeated multiple times, clear input and keep error as is
+            localStorage.clear();
+        }
+    }
+  }
+
   // Set display configurations
   var data = {
     labels: chartData?.time,
     datasets: [{
-      label: astheticNaming + ' Price',
+      label: astheticNaming + " Price",
       data: chartData?.res?.prices?.map(x => x[1].toFixed(2)),
       backgroundColor: 'red',
       borderColor: 'red',
@@ -148,10 +193,11 @@ const ERC720 = () => {
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h2>ERC20 Market Data</h2>
           </div>
+          { formAlert === "invalid" ? <div><Alert type="danger"/></div> : <div/> }
           <h3 style={{marginTop: '2rem'}}>{astheticNaming + " "} Price: <b>${coinInfo.information[Object.keys(coinInfo.information)[0]].usd} USD</b></h3> 
           <h5 style={{marginBottom: '2rem', display: 'inline'}}>24 Hr. % Change:
             { coinInfo.information[Object.keys(coinInfo.information)[0]].usd_24h_change < 0 ? 
-              <h5 style={{display: 'inline', color: 'red'}}>{" " + coinInfo.information[Object.keys(coinInfo.information)[0]].usd_24h_change.toFixed(2) +"%"}</h5> : 
+              <h5 style={{display: 'inline', color: 'red'}}>{" " + coinInfo.information[Object.keys(coinInfo.information)[0]].usd_24h_change.toFixed(2) + "%"}</h5> : 
               <h5 style={{display: 'inline', color: 'green'}}>{" +" + coinInfo.information[Object.keys(coinInfo.information)[0]].usd_24h_change.toFixed(2) + "%"}</h5>
             }
           </h5>
@@ -168,8 +214,14 @@ const ERC720 = () => {
               </div>
             }
           </div>
+          <form onSubmit={formHandler} style={{marginTop: '1.5rem'}}>
+            <label style={{marginRight: '0.5rem'}}>ERC20 Contract Address: </label>
+            <input type="text" onChange={(e) => updateContractAddress(e.target.value)} placeholder="Enter here" required />
+            <br />
+            <button style={{marginTop: '1rem'}} type="submit" class="btn btn-primary">Check Data</button>
+          </form>
           <div>
-            <button class="btn btn-success" style={{marginTop: '2rem'}} onClick={() => navigate("/")}>Go To Dashboard</button>
+            <button class="btn btn-success" style={{marginTop: '3rem'}} onClick={() => navigate("/")}>Go To Dashboard</button>
           </div>
         </main>
       </div>
