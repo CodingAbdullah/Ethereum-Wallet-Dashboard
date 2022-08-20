77334583ca620f56350ba4c564ce4ea9a87f8eb4
Abdul <abdullahmd326@gmail.com>
2022-08-19 22:45:03 -0400

    Address ---> ENS Resolver API added

--- frontend/src/Components/ENSPage/ENSPage.jsx
+++ frontend/src/Components/ENSPage/ENSPage.jsx
@@ -1,14 +1,45 @@
 import React, { useState } from 'react';
-
+import Alert from '../Alert/Alert';
 
 const ENSPage = () => {
 
     const [ensToAddress, updateENSToAddress] = useState('');
     const [addressToENS, updateAddressToENS] = useState('');
 
-    const formHandler = (e) => {
-        // ENS APIs go here..
+    const [setAlert, updateAlert] = useState(false);
 
+    const URL = "https://api.transpose.io/v0/ens"; 
+    const ENDPOINT = '/ens-records-by-owner'
+
+    const formHandler = (e) => {
+        e.preventDefault();
+
+        // ENS APIs go here.. Address ---> ENS Resolver first
+        if (updateAddressToENS.length === 42 && updateAddressToENS.substring(0, 2) === '0x'){
+            
+            fetch(URL + ENDPOINT, 
+                {   method: 'GET', 
+                    body: JSON.stringify({ owner_address: addressToENS }), 
+                    headers: { 
+                        'content-type' : 'application/json', 
+                        'X-API-KEY' : process.env.REACT_APP_TRANSPOSE_API_KEY // Transpose API key hidden 
+                    }
+                }
+            )
+            .then(response => response.json())
+            .then(res => { 
+                if (res.status === 'error'){
+                    updateAlert(true); // If response is 401, 404, or 500 set alert
+                }
+                else {
+                    if (setAlert) {
+                        updateAlert(false); // If Alert was set, remove it
+                    }    
+                    console.log(res);
+                }
+            })
+            .catch(err => console.error(err)); // Retrieve error
+        }
     }
 
     return (
@@ -19,6 +50,7 @@ const ENSPage = () => {
                 </div>
                 <form onSubmit={formHandler}>
                     <h4>ENS Lookups</h4>
+                    { setAlert ? <Alert type='danger' /> : null}
                     <label style= {{marginRight: '2rem'}}>{"ENS -> Address Resolver"}</label>
                     <input type="text" onChange={e => updateENSToAddress(e.target.value)} />
                     <br />
