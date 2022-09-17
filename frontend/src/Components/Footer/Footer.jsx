2fc92dbbfa0b92122c8f5c4b4b0b23e6c75afc09
Abdul <abdullahmd326@gmail.com>
2022-09-17 02:47:57 -0400

    Blocknative + Transpose APIs added

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -9,14 +9,15 @@ const Footer = () => {
             <div class="footer-container container">
                 <footer class="footer col-md-9 ml-sm-auto col-lg-10 px-md-4">
                     <div class="container">
-                        <p style={{ marginLeft: '2rem', marginTop: '-0.08rem' }}className="copyright-paragraph" >Powered By:  
-                            <a style={{ color:'black' }} href="https://moralis.io/"><img style={{ marginLeft: '0.25rem', marginRight: '0.15rem' }} src={require("../../assets/images/moralis.png")} width="25" height="25" alt="logo" />Moralis</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span>
-                            <a style={{ color:'black' }} href="https://www.coingecko.com"><img style={{ marginRight: '0.15rem' }} src={require("../../assets/images/coingecko.png")} width="25" height="25" alt="logo" />Coin Gecko</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span>
-                            <a style={{ color:'black' }} href="https://etherscan.io/"><img src={require("../../assets/images/etherscan.png")} width="25" height="25" alt="logo" />Etherscan</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span> 
-                            <a style={{ color:'black' }} href="https://ens.domains/"><img src={require("../../assets/images/ens.png")} width="25" height="25" alt="logo" />ENS</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span> 
-                            <a style={{ color:'black' }} href="https://opensea.io/"><img style={{ marginRight: '0.15rem' }} src={require("../../assets/images/opensea.png")} width="25" height="25" alt="logo" />Opensea</a>
+                        <p style={{ marginLeft: '2.5rem', marginTop: '-0.08rem' }} className="copyright-paragraph">
+                            <a style={{ color:'black' }} href="https://moralis.io/" target="_blank" rel="noreferrer"><img style={{ marginLeft: '0.25rem', marginRight: '0.15rem' }} src={require("../../assets/images/moralis.png")} width="25" height="25" alt="logo" />Moralis</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span>
+                            <a style={{ color:'black' }} href="https://www.coingecko.com" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.15rem' }} src={require("../../assets/images/coingecko.png")} width="25" height="25" alt="logo" />Coin Gecko</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span>
+                            <a style={{ color:'black' }} href="https://etherscan.io/" target="_blank" rel="noreferrer"><img src={require("../../assets/images/etherscan.png")} width="25" height="25" alt="logo" />Etherscan</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span> 
+                            <a style={{ color:'black' }} href="https://transpose.io/" target="_blank" rel="noreferrer"><img style={{marginRight: '0.15rem'}} src={require("../../assets/images/transpose.png")} width="20" height="20" alt="logo" />Transpose</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span> 
+                            <a style={{ color:'black' }} href="https://blocknative.com/" target="_blank" rel="noreferrer"><img style={{marginRight: '0.1rem'}} src={require("../../assets/images/blocknative.png")} width="25" height="25" alt="logo" />Blocknative</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span> 
+                            <a style={{ color:'black' }} href="https://opensea.io/" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.15rem' }} src={require("../../assets/images/opensea.png")} width="25" height="25" alt="logo" />Opensea</a>
                         </p>
-                        <p style={{ marginLeft: '2rem', marginTop: '-0.75rem' }} className="copyright-paragraph" >{statement} <a style={{ color: 'black' }} href="https://reactjs.org/">React</a><img className="react-logo" src={require("../../assets/images/logo.svg").default} alt="logo" /></p>
+                        <p style={{ marginLeft: '3.5rem', marginTop: '-0.75rem' }} className="copyright-paragraph" >{statement} <a style={{ color: 'black' }} href="https://reactjs.org/">React</a><img className="react-logo" src={require("../../assets/images/logo.svg").default} alt="logo" /></p>
                     </div>
                 </footer>
             </div>
