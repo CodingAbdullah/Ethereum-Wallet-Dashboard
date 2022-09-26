1404f2ff25f698c4ed3ae81dcce84f8d5bf6b1d3
Abdul <abdullahmd326@gmail.com>
2022-09-26 12:00:41 -0400

    EVM compatible chains link added, read-only testnets added

--- frontend/src/Components/NetworkSelector/NetworkSelector.jsx
+++ frontend/src/Components/NetworkSelector/NetworkSelector.jsx
@@ -4,23 +4,34 @@ import './NetworkSelector.css';
 const NetworkSelector = () => {
     const [selectedValue, updateSelectedValue] = useState('eth'); // Update network value
 
-    // Add in options for Mainnet, Goerli, Sepolia testnets. L2 ---> Polygon and their testnet, Mumbai
+    // Add in options for Mainnet, Kovan, Goerli, Rinkeby, Ropsten, Sepolia testnets. L2 ---> Polygon and their testnet, Mumbai
     return (
-        <div className='network-selector'>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">            
             <label>{selectedValue}</label>
+            <br/>
+            <label>
+                <b>Network Selector</b>
+            </label>
             <div className="form-check">
                 <input className="form-check-input" name='network-type' type="radio" onChange={() => updateSelectedValue('eth')} value="eth" />
-                <label className='network-label' class="form-check-label">Ethereum Mainnet</label>
+                <label id='network-label' class="form-check-label">Ethereum Mainnet</label>
+                <input className="form-check-input" name='network-type' type="radio" onChange={() => updateSelectedValue('kovan')} value="kovan" />
+                <label id='network-label' class="form-check-label">Kovan Testnet</label>
                 <input className="form-check-input" name='network-type' type="radio" onChange={() => updateSelectedValue('goerli')} value="goerli" />
-                <label className='network-label' class="form-check-label">Goerli Testnet</label>
+                <label id='network-label' class="form-check-label">Goerli Testnet</label>
+                <input className="form-check-input" name='network-type' type="radio" onChange={() => updateSelectedValue('rinkeby')} value="rinkeby" />
+                <label id='network-label' class="form-check-label">Rinkeby Testnet</label>
+                <br />
+                <input className="form-check-input" name='network-type' type="radio" onChange={() => updateSelectedValue('ropsten')} value="ropsten" />
+                <label id='network-label' class="form-check-label">Ropsten Mainnet</label>
                 <input className="form-check-input" name='network-type' type="radio" onChange={() => updateSelectedValue('sepolia')} value="sepolia" />
-                <label className='network-label' class="form-check-label">Sepolia Testnet</label>
+                <label id='network-label' class="form-check-label">Sepolia Testnet</label>
                 <input className="form-check-input" name='network-type' type="radio" onChange={() => updateSelectedValue('polygon')} value="polygon" />
-                <label className='network-label' class="form-check-label">Polygon Mainnet</label>
+                <label id='network-label' class="form-check-label">Polygon Mainnet</label>
                 <input className="form-check-input" name='network-type' type="radio" onChange={() => updateSelectedValue('polygon-mumbai')} value="polygon-mumbai" />
-                <label className="network-label">Polygon Mumbai Testnet</label>
+                <label id="network-label">Polygon Mumbai Testnet</label>
             </div>
-        </div>
+        </main>
     )
 }
 
