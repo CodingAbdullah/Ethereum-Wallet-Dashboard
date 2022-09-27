a7a64d1523f703794126e471f828fd00c573b8a2
Abdul <abdullahmd326@gmail.com>
2022-09-27 02:02:12 -0400

    Adding Utility Modules for backend services for testnets

---
+++ backend/Utils/APIURLReference.js
@@ -0,0 +1,4 @@
+// Fixed URLs to be accessed from one point of reference
+export const BLOCKNATIVE_URL = 'https://api.blocknative.com/gasprices/blockprices';
+export const MORALIS_URL = 'https://deep-index.moralis.io/api/v2/';
+export const ALCHEMY_URL = 'https://eth-mainnet.g.alchemy.com/nft/v2';
---
+++ backend/Utils/NetworkMapper.js
@@ -0,0 +1,12 @@
+// To be used to configure which blockchain network to access information
+export const NETWORK_MAPPER = {
+    'eth' : 'https://api.etherscan.io/api',
+    'goerli' : 'https://api-goerli.etherscan.io/api',  
+    'kovan' : 'https://api-kovan.etherscan.io/api',
+    'rinkeby' : 'https://api-rinkeby.etherscan.io/api',
+    'ropsten' : 'https://api-ropsten.etherscan.io/',
+    'sepolia' : 'https://api-sepolia.etherscan.io/api',
+    'polygon' :  'https://api.polygonscan.com/api',
+    'polygon-mumbai' : 'https://api-testnet.polygonscan.com/api'
+}
+
