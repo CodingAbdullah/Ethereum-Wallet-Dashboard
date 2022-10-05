f2679941b3066f60eb313c56d692d451254524eb
Abdul <abdullahmd326@gmail.com>
2022-10-05 07:51:15 -0400

    Deployment ready for AWS

--- backend/Routes/GasTrackerRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const GasTrackerController = require("../Controller/GasTrackerController");
-
-// Add routes for Gas Track information
-router.get('/gas-track', GasTrackerController.gasTrackInformation);
-
-module.exports = router;
