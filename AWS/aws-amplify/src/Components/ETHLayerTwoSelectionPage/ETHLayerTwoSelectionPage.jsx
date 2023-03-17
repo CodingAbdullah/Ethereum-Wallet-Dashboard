ef731ceecdd3dc7314f00080c8a5da2e983f06ab
Abdul <abdullahmd326@gmail.com>
2023-03-17 00:11:02 -0400

    Adding AWS Amplify link for Optimism Board to main board

--- AWS/aws-amplify/src/Components/ETHLayerTwoSelectionPage/ETHLayerTwoSelectionPage.jsx
+++ AWS/aws-amplify/src/Components/ETHLayerTwoSelectionPage/ETHLayerTwoSelectionPage.jsx
@@ -16,7 +16,7 @@ const ETHLayerTwoSelectionPage = () => {
                         <div>
                             <label>Arbitium</label><a href="https://arbitrum-aws.d1i5adqmyft713.amplifyapp.com/" target="_blank" rel="noreferrer"><button style={{ marginLeft: '5.5rem' }} class="btn btn-success">View</button></a>
                             <hr style={{ marginTop: '2rem', marginBottom: '2rem' }} />    
-                            <label>Optimism</label><button onClick={ () => navigate( "/optimism-w-dashboard" )} style={{ marginLeft: '5.0rem' }} class='btn btn-success'>View</button>
+                            <label>Optimism</label><a href="https://optimism-aws.d22w7ozmz21la.amplifyapp.com/" target="_blank" rel="noreferrer"><button style={{ marginLeft: '5.0rem' }} class="btn btn-success">View</button></a>
                             <hr style={{ marginTop: '2rem', marginBottom: '2rem' }} />     
                             <label>Immutable X</label><button onClick={ () => navigate( "/imx-w-dashboard" )} style={{ marginLeft: '3.75rem' }} class='btn btn-success'>View</button>
                             <hr style={{ marginTop: '2rem', marginBottom: '2rem' }} />
