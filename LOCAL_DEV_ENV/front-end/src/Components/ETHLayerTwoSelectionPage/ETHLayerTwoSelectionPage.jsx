a5d3d149acec6c6fc62f9c4ac13b933cc3b0d022
Abdul <abdullahmd326@gmail.com>
2023-03-30 00:21:01 -0400

    Adding IMX link to dashboard

--- LOCAL_DEV_ENV/front-end/src/Components/ETHLayerTwoSelectionPage/ETHLayerTwoSelectionPage.jsx
+++ LOCAL_DEV_ENV/front-end/src/Components/ETHLayerTwoSelectionPage/ETHLayerTwoSelectionPage.jsx
@@ -18,7 +18,7 @@ const ETHLayerTwoSelectionPage = () => {
                             <hr style={{ marginTop: '2rem', marginBottom: '2rem' }} />    
                             <label>Optimism</label><a href="https://optimism-aws.d22w7ozmz21la.amplifyapp.com/" target="_blank" rel="noreferrer"><button style={{ marginLeft: '5.0rem' }} class="btn btn-success">View</button></a>
                             <hr style={{ marginTop: '2rem', marginBottom: '2rem' }} />     
-                            <label>Immutable X</label><button onClick={ () => navigate( "/imx-w-dashboard" )} style={{ marginLeft: '3.75rem' }} class='btn btn-success'>View</button>
+                            <label>Immutable X</label><a href="https://www.immutable.com/developers" target="_blank" rel="noreferrer"><button style={{ marginLeft: '3.5rem' }} class="btn btn-success">View</button></a>
                             <hr style={{ marginTop: '2rem', marginBottom: '2rem' }} />
                             <label>Polygon</label><button onClick={ () => navigate( "/polygon-w-dashboard" )} style={{ marginLeft: '5.5rem' }} class='btn btn-success'>View</button>
                         </div>
