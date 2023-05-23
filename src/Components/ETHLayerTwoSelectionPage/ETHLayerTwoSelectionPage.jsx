622645592664b8eedce9084ad964603c7660f8f0
Abdul <abdullahmd326@gmail.com>
2023-05-23 14:50:31 -0400

    Updating Arbitium -> Arbitrum

--- src/Components/ETHLayerTwoSelectionPage/ETHLayerTwoSelectionPage.jsx
+++ src/Components/ETHLayerTwoSelectionPage/ETHLayerTwoSelectionPage.jsx
@@ -14,7 +14,7 @@ const ETHLayerTwoSelectionPage = () => {
                 <div class="jumbotron">
                     <div class="container">
                         <div>
-                            <label>Arbitium</label><a href="https://arbitrum-aws.d1pqf6famiyi96.amplifyapp.com/" target="_blank" rel="noreferrer"><button style={{ marginLeft: '5.5rem' }} class="btn btn-success">View</button></a>
+                            <label>Arbitrum</label><a href="https://arbitrum-aws.d1pqf6famiyi96.amplifyapp.com/" target="_blank" rel="noreferrer"><button style={{ marginLeft: '5.5rem' }} class="btn btn-success">View</button></a>
                             <hr style={{ marginTop: '2rem', marginBottom: '2rem' }} />    
                             <label>Optimism</label><a href="https://optimism-aws.d22w7ozmz21la.amplifyapp.com/" target="_blank" rel="noreferrer"><button style={{ marginLeft: '5.0rem' }} class="btn btn-success">View</button></a>
                             <hr style={{ marginTop: '2rem', marginBottom: '2rem' }} />     
