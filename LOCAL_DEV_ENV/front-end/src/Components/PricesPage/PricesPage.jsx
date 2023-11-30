90693552860ff601863da9529045d7c153d7509e
Abdul <abdullahmd326@gmail.com>
2023-11-30 01:21:54 -0500

    Adding in React Query Functions using CoinGecko PRO API

--- LOCAL_DEV_ENV/front-end/src/Components/PricesPage/PricesPage.jsx
+++ LOCAL_DEV_ENV/front-end/src/Components/PricesPage/PricesPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { coinPrices } from '../../UtilFunctions/coinPrices';
+import { coinPricesPro } from '../../UtilFunctions/coinPricesPro';
 import PriceCoinCard from '../PriceCoinCard/PriceCoinCard';
 
 const PricesPage = () => {
     // Incorporating React Query for faster and more efficient data fetch
     const { data, isPending, isError, error } = useQuery({
         queryKey: ['coin prices'],
-        queryFn: coinPrices
+        queryFn: coinPricesPro
      });
 
     // Props to be added later after more filtering and testing, the layout is complete for now
