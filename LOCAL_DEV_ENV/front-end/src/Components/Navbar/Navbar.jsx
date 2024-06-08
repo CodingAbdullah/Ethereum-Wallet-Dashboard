fd9acc69449436a79cff050a3223b454204b63a9
CodingAbdullah <abdullahmd326@gmail.com>
2024-06-08 04:29:57 -0400

    Navbar debuggging complete

--- LOCAL_DEV_ENV/front-end/src/Components/Navbar/Navbar.jsx
+++ LOCAL_DEV_ENV/front-end/src/Components/Navbar/Navbar.jsx
@@ -33,11 +33,11 @@ const Navbar = () => {
                                 <a style={ styles.navLinkColor } className="nav-link" aria-current="page" href="/about">About</a>
                             </li>
                             <li className="nav-item dropdown">
-                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="prices" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
                                     Prices
                                 </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <ul className="dropdown-menu" aria-labelledby="prices">
                                     {
                                         isFreeVersion ? null :                            
                                             <li className="nav-item">
@@ -48,31 +48,31 @@ const Navbar = () => {
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
-                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="holdings" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
                                     Token Holdings
                                 </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <ul className="dropdown-menu" aria-labelledby="holdings">
                                     <li><a className="dropdown-item" href="/erc20-holdings">ERC20 Holdings</a></li>
                                     <li><a className="dropdown-item" href="/erc721-holdings">ERC721 Holdings</a></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
-                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="lookups" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
                                     Token Lookups
                                 </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <ul className="dropdown-menu" aria-labelledby="lookups">
                                     <li><a className="dropdown-item" href="/ens-lookup">ENS</a></li>
                                     <li><a className="dropdown-item" href="/erc721-lookups">ERC721 Token Lookups</a></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
-                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="dashboards" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
                                     Layer 2 Dashboards
                                 </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <ul className="dropdown-menu" aria-labelledby="dashboards">
                                     <li><a className="dropdown-item" target="_blank" href="https://arbitrum-aws.d1pqf6famiyi96.amplifyapp.com/">Arbitrum</a></li>
                                     <li><a className="dropdown-item" target="_blank" href="https://optimism-aws.d22w7ozmz21la.amplifyapp.com/">Optimism</a></li>                                    
                                     <li><a className="dropdown-item" target="_blank" href="https://aws.d2n4l9is533l0n.amplifyapp.com/">Polygon</a></li>
@@ -82,11 +82,11 @@ const Navbar = () => {
                                 <a style={ styles.navLinkColor } className="nav-link" href="/gas-tracker">Gas Station</a>
                             </li>
                             <li className="nav-item dropdown">
-                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                <a style={ styles.navLinkColor } className="nav-link dropdown-toggle" href="#" id="analytics" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
                                     Analytics
                                 </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <ul className="dropdown-menu" aria-labelledby="analytics">
                                     <li><a className="dropdown-item" href="/collections">Token Analytics</a></li>
                                     <li><a className="dropdown-item" href="/walletAnalytics">Wallet Analytics</a></li>
                                 </ul>
