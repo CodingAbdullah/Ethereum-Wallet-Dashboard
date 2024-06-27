9c854395306b959b182df1eb1b014bd7d4df829b
CodingAbdullah <abdullahmd326@gmail.com>
2024-06-27 04:14:12 -0400

    Gas prices feature refactored and updated

--- AWS/aws-ec2/Controller/GasTrackerController.js
+++ AWS/aws-ec2/Controller/GasTrackerController.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: '../.env' });
 const BLK_URL = require('../Utils/NetworkMapper').NETWORK_MAPPER.blocknative_url;
 const axios = require('axios');
 
+// Controller function for fetching Ethereum gas information
 exports.gasTrackInformation = (req, res) => {
 
     // Add blocknative credentials
@@ -12,15 +13,16 @@ exports.gasTrackInformation = (req, res) => {
             'Authorization' : process.env.BLK_API_KEY
         }
     }
+    
     axios.get(BLK_URL, options)
     .then(response => {
         res.status(200).json({
             information: response.data
-        })
+        });
     })
     .catch(err => {
         res.status(400).json({
             information: err
-        })
-    })
+        });
+    });
 }
--- AWS/aws-ec2/Routes/GasTrackerRouter.js
+++ AWS/aws-ec2/Routes/GasTrackerRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const GasTrackerController = require("../Controller/GasTrackerController");
 
-// Add routes for Gas Track information
+// Add routes for gas track
 router.get('/gas-track', GasTrackerController.gasTrackInformation);
 
 module.exports = router;
