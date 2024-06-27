d0d79bf8b55d45b1f20b8875b3d06ec70cab7b71
Abdullah Muhammad <abdullahmd326@gmail.com>
2024-06-27 04:17:54 -0400

    Gas prices page refactored and updated

--- src/Components/GasTrackerPage/GasTrackerPage.jsx
+++ src/Components/GasTrackerPage/GasTrackerPage.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { metricsNavbarGasPrice } from '../../UtilFunctions/metricsNavbarGasPrice';
 import { useNavigate } from 'react-router';
-import GasBaseFeeCard from '../GasBaseFeeCard/GasBaseFeeCard';
-import GasBlockPriceCard from '../GasBlockPriceCard/GasBlockPriceCard';
+import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
+import GasBlockInfoTable from '../GasBlockInfoTable/GasBlockInfoTable';
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 
 const GasTrackerPage = () => {
     // Setting up query to fetch Ethereum gas price
@@ -23,43 +25,36 @@ const GasTrackerPage = () => {
     else if (gasPriceQuery.isSuccess){ 
         // Using parent-child component hierarchy, pass down state information for display and leaner code
         let gas = gasPriceQuery.data[0].information;
+
+        let rowData = [];
+        
+        // Inserting data to be represented as rows within the table
+        rowData.push({ generalMetrics: 'System', data: gas.system.substring(0, 1).toUpperCase() + gas.system.substring(1) });
+        rowData.push({ generalMetrics: 'Network', data: gas.network.substring(0, 1).toUpperCase() + gas.network.substring(1) });
+        rowData.push({ generalMetrics: 'Unit', data: gas.unit.substring(0, 1).toUpperCase() + gas.unit.substring(1) });
+        rowData.push({ generalMetrics: 'Max Price', data: gas.maxPrice + ' Gwei' });
+        rowData.push({ generalMetrics: 'Current Block Number', data: gas.currentBlockNumber });
+        rowData.push({ generalMetrics: 'MsSinceLastBlock', data: gas.msSinceLastBlock });
+
+        let columnData = [
+            { field: "generalMetrics", headerName: 'General Metrics', flex: 1 },
+            { field: 'data', headerName: 'Data', flex: 1 } 
+        ];
+
+        // Rendering tables and child components containing Ethereum information
         return (
             <div className='gas-tracker-page'>
                 <main role="main" class="p-3">
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2">Gas Information</h1>
                     </div>
-                    <div class="jumbotron">
-                        <h4 style={{ marginTop: '-1rem' }} class="display-5">General Metrics</h4>
-                        <div style={{ marginTop: '3rem', marginBottom: '-2rem' }} class="container">
-                            <p>System: <b>{ gas.system.substring(0, 1).toUpperCase() + gas.system.substring(1) }</b></p>
-                            <p>Network: <b>{ gas.network.substring(0, 1).toUpperCase() + gas.network.substring(1) }</b></p>
-                            <p>Unit: <b>{ gas.unit.substring(0 ,1).toUpperCase() + gas.unit.substring(1) }</b></p>
-                            <p>Max Price: <b>{ gas.maxPrice }</b></p>
-                            <p>Current Block Number: <b>{ gas.currentBlockNumber }</b></p>
-                            <p>MsSinceLastBlock: <b>{ gas.msSinceLastBlock }</b></p>
-                        </div>
+                    <p><b>Ecosystem Overview</b><br /><i>High end look at the blockchain</i></p>
+                    <div className="ag-theme-quartz" style={{ marginLeft: 'auto', marginRight: 'auto', height: 200, width: '100%' }}>
+                        <AgGridReact
+                            rowData={rowData}
+                            columnDefs={columnData} />
                     </div>
-                    <div style={{ marginTop: '2rem' }} class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h3 class="h3">Block Prices</h3>
-                    </div> 
-                    <div style={{ marginTop: '2rem' }} class="row">
-                        { gas.blockPrices[0].estimatedPrices.map((block, key) => {
-                            return (
-                                <GasBlockPriceCard id={ key } information={ block }/>
-                            )
-                        })}
-                    </div>
-                    <div style={{ marginTop: '4rem' }} class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h3 class="h3">Base Fees</h3>
-                    </div>  
-                    <div style={{ marginTop: '2rem' }} class="row">
-                        { gas.estimatedBaseFees.map((base, key) => {
-                            return (
-                                <GasBaseFeeCard id={ key } information={ base } />
-                            )
-                        })}
-                    </div>
+                    <GasBlockInfoTable information={ gas.blockPrices[0].estimatedPrices }/>
                     <div>
                         <button style={{ marginTop: '3rem'}} onClick={ () => navigate("/") } class='btn btn-success'>Go To Dashboard</button>
                     </div>
