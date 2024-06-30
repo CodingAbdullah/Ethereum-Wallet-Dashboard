187e06b34cf7425ef470f593ad87ff86b12d0b8b
Abdullah Muhammad <abdullahmd326@gmail.com>
2024-06-29 20:40:33 -0400

    Market Cap, Volume, and Floor Price data added to ERC721 Collection Page

--- src/Components/DurationSelector/DurationSelector.jsx
+++ src/Components/DurationSelector/DurationSelector.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const DurationSelector = (props) => {
 
+    let location = window.location.pathname;
+
     // Set options for price duration calculation
     let optionsValue = (
         <select onChange={ props.priceDurationHandler } className="form-select" aria-label="Default select example">
-            <option selected value="24">Last 24 Hours</option>
-            <option value="7">Last 7 Days</option>
+            { location === '/collections/erc721-collection' ? null : <option selected value="24">Last 24 Hours</option> }
+            { location === '/collections/erc721-collection' ? null : <option value="7">Last 7 Days</option> }
             <option value="14">Last 14 Days</option>
             <option value="30">Last 30 days</option>
         </select>
