801a5ba9e0d9a35ebc0e11ccaaf58d6b0e80f5c9
CodingAbdullah <abdullahmd326@gmail.com>
2024-10-23 00:20:55 -0400

    Table formatting Top/Bottom coins

--- AWS/aws-amplify/src/Components/BottomCoinsInfoTable/BottomCoinsInfoTable.jsx
+++ AWS/aws-amplify/src/Components/BottomCoinsInfoTable/BottomCoinsInfoTable.jsx
@@ -55,6 +55,66 @@ const BottomCoinsInfoTable = (props) => {
             }
          }
     ]);
+    
+    // Function for handling column renders on window screen size
+    const updateColumnDefs = () => {
+        if (window.outerWidth < 800) {
+            setColumnDefs([
+                { field: "name", headerName: 'Name', flex: 1,
+                    cellRenderer: (params) => {
+                        let filteredID = coinTableRowData.filter(coin => coin.name === params.value)[0].id;
+                        return (
+                            <a style={{ color: 'black' }} href="/chart" onClick={ () => dispatch(selectCoin(filteredID)) }>{ params.value }</a>
+                        )
+                    }
+                 },
+                { field: "symbol", headerName: "Symbol", flex: 1, 
+                    cellRenderer: (params) => {
+                        return (
+                            <p><img src={ params.value.split(" - ")[1] } alt="Thumbnail" style={{ width: '20px', height: '20px' }} />{" "}{ params.value.split(" - ")[0] }</p>
+                        )
+                    }
+                },
+                { field: "currentPrice", headerName: "Price", flex: 1 },
+                { field: "marketCap", headerName: "Market Cap Rank", flex: 1 }
+            ]);
+        } 
+        else {
+            setColumnDefs([
+                { field: "name", headerName: 'Name', flex: 1,
+                    cellRenderer: (params) => {
+                        let filteredID = coinTableRowData.filter(coin => coin.name === params.value)[0].id;
+                        return (
+                            <a style={{ color: 'black' }} href="/chart" onClick={ () => dispatch(selectCoin(filteredID)) }>{ params.value }</a>
+                        )
+                    }
+                 },
+                { field: "symbol", headerName: "Symbol", flex: 1, 
+                    cellRenderer: (params) => {
+                        return (
+                            <p><img src={ params.value.split(" - ")[1] } alt="Thumbnail" style={{ width: '20px', height: '20px' }} />{" "}{ params.value.split(" - ")[0] }</p>
+                        )
+                    }
+                },
+                { field: "currentPrice", headerName: "Price", flex: 1 },
+                { field: "marketCap", headerName: "Market Cap Rank", flex: 1 },
+                { field: "percentageChange24Hours", headerName: "24 Hr % Change", flex: 1,
+                    cellRenderer: (params) => {
+                        return (
+                            <p style={{ color: 'green' }}><b>{params.value}</b></p>
+                        )
+                    }
+                }                      
+            ]);
+        }
+    }
+        
+    // Dynamically adjust table size depending on screen size
+    useEffect(() => {
+        updateColumnDefs();
+        window.addEventListener('resize', updateColumnDefs);
+        return () => window.removeEventListener('resize', updateColumnDefs);
+    }, []);
 
     // Render Ag-Grid React component with row and column data
     // Display coin price data
