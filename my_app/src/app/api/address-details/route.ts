6c93cbeedbe0d9019181510997e58be566150836
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-08 06:42:24 -0500

    Refactoring address-details back-end route codebase

--- my_app/src/app/api/address-details/route.ts
+++ my_app/src/app/api/address-details/route.ts
@@ -11,14 +11,13 @@ const tag = "latest";
 export default async function POST(request: Request){
     const body = await request.json(); // Retrieve information from request
 
-    if (body.network === 'eth' || body.network === 'sepolia' || body.network.split('-')[0] === 'polygon'){
-        
+    if (body.network === 'eth' || body.network === 'sepolia'){
         // Ensure network belongs to as one of the keys in the NETWORK_MAPPER constant
         const network: keyof typeof NETWORK_MAPPER = body.network; // Type assertion for network
         
         // Gather wallet analytics using API resources and running checks to see if wallet address is valid
         const data = await fetch(NETWORK_MAPPER[network] + "?module=" + mod + "&action=" + action + "&address=" + body.address + "&tag=" + tag + "&apikey=" + 
-        ((body.network === 'polygon' || body.network === 'polygon-mumbai') ? process.env.POLYGON_API_KEY : process.env.ETHERSCAN_API_KEY))
+         process.env.ETHERSCAN_API_KEY)
             
         // Fetch data using the Ethereum data endpoints
         if (!data.ok) 
