a1f29e789d96f0b3f6bba49c27ce342749ade82b
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-08 06:32:25 -0500

    Adding Network Mapper constant and type to codebase

--- my_app/src/app/utils/constants/NETWORK_MAPPER.ts
+++ my_app/src/app/utils/constants/NETWORK_MAPPER.ts
@@ -1,18 +1,14 @@
+import NetworkMapperType from "../types/NetworkMapperType";
+
 /*  
     To be used to configure which blockchain network to access information
     ETH and their testnet Sepolia will be fetched using Etherscan
     Moralis will be used for the other testnets on Ethereum
     Polygonscan for their mainnet and their Mumbai testnet
 */
-exports.NETWORK_MAPPER = {
+export const NETWORK_MAPPER: NetworkMapperType = {
     'eth' : 'https://api.etherscan.io/api',
-    'goerli' : 'https://api-goerli.etherscan.io/api',  
-    'kovan' : 'https://api-kovan.etherscan.io/api',
-    'ropsten' : 'https://api-ropsten.etherscan.io/api',
-    'rinkeby' : 'https://api-rinkeby.etherscan.io/api',
     'sepolia' : 'https://api-sepolia.etherscan.io/api',
-    'polygon' :  'https://api.polygonscan.com/api',
-    'polygon-mumbai' : 'https://api-testnet.polygonscan.com/api',
     'alchemy_url' : 'https://eth-mainnet.g.alchemy.com/nft/v2',    
     'blocknative_url' : 'https://api.blocknative.com/gasprices/blockprices',
     'moralis_url' : 'https://deep-index.moralis.io/api/v2/',
