f3a41c4acf88826adbd664c14e298c340d39768c
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-08 08:54:18 -0500

    ERC721 Sales back-end route complete

---
+++ my_app/src/app/api/erc721-sales-by-id/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+// Custom Route Handler function
+export default async function POST(request: Request){
+    const body = await request.json(); // Retrieve information from request
+    const { address, id } = body;
+
+    // Set params
+    const params = {
+        "chain_id": "ethereum",
+        "contract_address": address,
+        "token_id": id
+    }
+    
+    // Set options for data request
+    const options = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'accept' : 'application/json',
+            'X-API-KEY' : process.env.TRANSPOSE_API_KEY_1
+        } as HeadersInit
+    }
+
+    // Making request to Transpose API for finding ERC721 token sales information
+    const data = await fetch("https://api.transpose.io/nft/sales-by-token-id?" + new URLSearchParams(params), options)
+    
+    // Conditionally return data based on fetch request
+    if (data.ok) {
+        const information = await data.json();
+        NextResponse.json({
+            information
+        });
+    }
+    else {
+        NextResponse.json({}, { status: 400 });
+    }
+}
