be28987372d4f34af554e33fb46d92b30866536c
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-12 03:26:17 -0500

    Gas Tracker page complete

--- my_app/src/app/gas-tracker/page.tsx
+++ my_app/src/app/gas-tracker/page.tsx
@@ -1,6 +1,11 @@
+import GenericGasMetricsInfoTable from "../components/GenericGasMetricsInfoTable";
+import GasBlockInfoTable from "../components/GasBlockInfoTable";
+
 // Gas Tracker Page
-export default async function GasTrackerPage() {
-    // Render the Gas Tracker Page Component
+export default function GasTrackerPage() {
+
+    // Render the Gas Tracker Page
+    // Include the Generic Gas Metrics Table and the Gas Block Info Table Components
     return (
         <div className="min-h-screen bg-gray-950 text-gray-100 flex flex-col items-center justify-center">
             <div className="container mx-auto px-4 py-16 w-full max-w-3xl">
@@ -12,6 +17,10 @@ export default async function GasTrackerPage() {
                 <p className="text-xl text-gray-400 mb-12 text-center">
                     Dive deep into Ethereum Gas metrics
                 </p>
+                <hr className='p-3' /> 
+                <GenericGasMetricsInfoTable  />
+                <hr className='p-3' />
+                <GasBlockInfoTable />
             </div>
         </div>
     )
