f919346cce458655927b427ca9d391c0cd5902bd
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-12 08:42:04 -0500

    Back-end route for handling global market cap chart data refactored

--- my_app/src/app/api/global-market-cap-chart-data/route.ts
+++ my_app/src/app/api/global-market-cap-chart-data/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
+import dayjs from "dayjs";
 
 // Pro CoinGecko API Endpoint
 const PRO_COINGECKO_URL = "https://pro-api.coingecko.com/api/v3";
 
 // Custom Route Handler function
-export default async function GET(){
+export async function GET(){
 
     // Endpoint for fetching Global Cryptocurrency Market Cap data in the last 30 days
     const MARKET_DATA_ENDPOINT = "/global/market_cap_chart?days=30";
@@ -20,12 +21,21 @@ export default async function GET(){
     };
 
     // Pass in options containing API Key to make authenticated call to get market cap data
-    const data = await fetch(PRO_COINGECKO_URL + MARKET_DATA_ENDPOINT, options);
+    const response = await fetch(PRO_COINGECKO_URL + MARKET_DATA_ENDPOINT, options);
     
     // Fetch data using the Ethereum data endpoints
-    if (!data.ok) 
+    if (!response.ok) 
         return NextResponse.json({ error: 'Failed to fetch Ethereum price' }, { status: 500 });
     else {
-        return NextResponse.json(data);
+        // Return modified date/price data
+        const data = await response.json();
+        const marketCapData: [string, string] = data.market_cap_chart.market_cap;               
+        
+        return NextResponse.json({
+            capValues: marketCapData.map(cap => ({ 
+                date: dayjs(cap[0]).format('YYYY-MM-DD HH:mm:ss').split(" ")[0], 
+                price: Number(Number(cap[1])) 
+            }))
+        });
     }
 }
