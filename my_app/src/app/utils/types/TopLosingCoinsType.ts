fc3c3869a779a4e4904781f16dc924fe804402bf
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-14 00:18:13 -0500

    Top Losing Coins Data Table added

---
+++ my_app/src/app/utils/types/TopLosingCoinsType.ts
@@ -0,0 +1,9 @@
+// Top Losing Coins Data Type
+export default interface TopLosingCoinsType {
+    name: string,
+    symbol: string,
+    image: string,
+    usd: number,
+    market_cap_rank: number,
+    usd_24h_change: number
+}
