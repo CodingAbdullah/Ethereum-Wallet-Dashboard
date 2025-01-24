5352d7d737b340746400371ed13ffe03c62ea05e
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-24 04:48:32 -0500

    Generic detailed trending collection page added

---
+++ my_app/src/app/collections/trending-collections/[collection]/page.tsx
@@ -0,0 +1,42 @@
+import ERC721CollectionAttributeSummaryInfoTable from "@/app/components/ERC721CollectionAttributeSummaryInfoTable";
+import ERC721CollectionDataInfoTable from "@/app/components/ERC721CollectionDataInfoTable";
+import ERC721CollectionExtraDataInfoTable from "@/app/components/ERC721CollectionExtraDataInfoTable";
+import ERC721CollectionFloorPriceInfoTable from "@/app/components/ERC721CollectionFloorPriceInfoTable";
+import ERC721CollectionSalesInfoTable from "@/app/components/ERC721CollectionSalesInfoTable";
+import ERC721CollectionTransfersInfoTable from "@/app/components/ERC721CollectionTransfersInfoTable";
+import addressValidator from "@/app/utils/functions/addressValidator";
+
+// Displaying historical price information of a particular coin
+export default async function TrendingCollectionsPage({ params }: { params: { collection: string }}) {
+    const parameters = await params;
+    const collection = parameters.collection;
+
+    // Check validity of this coin by running a custom function validating if it exists within the Coin Gecko coin list
+    const validateCollection = await addressValidator(collection);
+
+    if (validateCollection) {
+        // Render the Generic Chart Page componen if the coin ID is valid
+        return (
+            <div className="min-h-screen bg-gray-800 text-gray-300 py-10 px-4 sm:px-6 lg:px-8 shadow-lg">
+                <h1 className="text-5xl font-bold mb-6 text-center">
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-400 to-gray-100">
+                        Trending Collection Data
+                    </span>
+                </h1>
+                <p className="text-xl text-gray-400 mb-12 text-center">
+                    <i>{collection}</i>
+                </p>
+                <ERC721CollectionDataInfoTable address={collection} />
+                <ERC721CollectionFloorPriceInfoTable address={collection} />
+                <ERC721CollectionExtraDataInfoTable address={collection} />
+                <ERC721CollectionAttributeSummaryInfoTable address={collection} />
+                <ERC721CollectionTransfersInfoTable address={collection} />
+                <ERC721CollectionSalesInfoTable address={collection} />
+            </div>
+        )
+    }   
+    else {
+        // Collection ID is not valid, therefore return the error page
+        throw new Error();
+    }
+}
