2d1d545d9de8757f5809031773ca300f86658909
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-26 01:16:13 -0500

    Pwsh/Shell scripts, AWS directory removed

--- AWS/aws-ec2/Routes/GlobalDefiDataRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const GlobalDefiDataController = require("../Controller/GlobalDefiDataController");
-
-// Add routes for ERC721 lookups
-router.get('/global-defi-data', GlobalDefiDataController.globalDefiData);
-
-module.exports = router;
--- AWS/aws-ec2/Routes/MarketDataRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const MarketDataController = require("../Controller/MarketDataController");
-
-// Add route for Global Market Cap Data
-router.get('/global-market-cap-chart-data', MarketDataController.globalMarketCapData);
-
-module.exports = router;
