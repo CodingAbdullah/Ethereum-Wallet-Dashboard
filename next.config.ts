e5a2f46e8fa0549b3d2289b1b19fe181ad15e3e2
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-31 17:10:27 -0500

    CDN Moralis domain added to Next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['coin-images.coingecko.com', 'logo.moralis.io']
+    domains: ['coin-images.coingecko.com', 'logo.moralis.io', 'cdn.moralis.io']
   }
 };
 
