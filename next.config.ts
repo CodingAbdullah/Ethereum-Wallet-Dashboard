fab568ede6c2d4e6e72e9da045b9a3cfd9833432
CodingAbdullah <abdullahmd326@gmail.com>
2025-01-31 17:13:53 -0500

    AWS S3 image bucket URL added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['coin-images.coingecko.com', 'logo.moralis.io', 'cdn.moralis.io']
+    domains: ['coin-images.coingecko.com', 'logo.moralis.io', 'cdn.moralis.io', 'market-data-images.s3.us-east-1.amazonaws.com']
   }
 };
 
