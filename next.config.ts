b4bfd1a30bc2ba5bc3d4039a8692dc0563c27c36
CodingAbdullah <abdullahmd326@gmail.com>
2026-05-03 00:07:49 -0400

    NPM packages and Next app configurations upgraded

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['coin-images.coingecko.com', 'logo.moralis.io', 'cdn.moralis.io', 'market-data-images.s3.us-east-1.amazonaws.com']
-  }
+    remotePatterns: [
+      { protocol: 'https', hostname: 'coin-images.coingecko.com' },
+      { protocol: 'https', hostname: 'logo.moralis.io' },
+      { protocol: 'https', hostname: 'cdn.moralis.io' },
+      { protocol: 'https', hostname: 'market-data-images.s3.us-east-1.amazonaws.com' },
+      { protocol: 'https', hostname: '**.seadn.io' },
+      { protocol: 'https', hostname: 'openseauserdata.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: false,
+    contentDispositionType: 'attachment',
+  },
+  experimental: {
+    optimizePackageImports: [
+      '@fortawesome/react-fontawesome',
+      '@fortawesome/free-solid-svg-icons',
+      '@fortawesome/free-regular-svg-icons',
+      '@fortawesome/free-brands-svg-icons',
+      'lucide-react',
+      'recharts',
+    ],
+  },
 };
 
 export default nextConfig;
